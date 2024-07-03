0085278d427cd9ff97cc065c015a4d49462cc41e
WhiteBearM <bhs9641@gmail.com>
2024-07-03 17:40:50 +0900

    route 위치 변경

--- nowa/src/App.tsx
+++ nowa/src/App.tsx
@@ -1,27 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<></>} />
-          <Route path="/login" element={<></>} />
-          <Route path="/register" element={<></>} />
-          <Route path="/find" element={<></>} />
-          <Route path="/mypage" element={<></>} />
-          <Route path="/contentUpload" element={<></>} />
-          <Route path="/contentDetail" element={<></>} />
-          <Route path="/contentEdit" element={<></>} />
-          <Route path="/profileEdit" element={<></>} />
-          <Route path="/chat" element={<></>} />
-          <Route path="/notification" element={<></>} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+  return <></>
 }
 
 const rootElement = document.getElementById('root')
--- nowa/src/routes/index.tsx
+++ nowa/src/routes/index.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 
-export default function index() {
+function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<></>} />
+          <Route path="/login" element={<></>} />
+          <Route path="/register" element={<></>} />
+          <Route path="/find" element={<></>} />
+          <Route path="/mypage" element={<></>} />
+          <Route path="/contentUpload" element={<></>} />
+          <Route path="/contentDetail" element={<></>} />
+          <Route path="/contentEdit" element={<></>} />
+          <Route path="/profileEdit" element={<></>} />
+          <Route path="/chat" element={<></>} />
+          <Route path="/notification" element={<></>} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
+
+export default App
