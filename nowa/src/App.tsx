3d341957b99709ad4241c048a372fd6949a207f8
WhiteBearM <bhs9641@gmail.com>
2024-07-09 17:57:06 +0900

    daisyUI Theme없을 경우  다크모드 고정되는 문제 해결

--- nowa/src/App.tsx
+++ nowa/src/App.tsx
@@ -2,21 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Routers from './routes'
 import '@/styles/tailwind.css'
-import Theme from '@/components/ThemeController/ThemeContoller'
-import Button from './components/Button/button'
-import TextArea from './components/TextArea/textArea'
-import IconLogin from '@/components/IconLogin/iconLogin'
-import Search from './components/Search/search'
-import TextInput from './components/TextInput/textInput'
-import FileInput from './components/FileInput/fileInput'
-import Tap from './components/Tap/tap'
+import Custom_Theme from '@/hooks/defaultTheme'
 
 const App: React.FC = () => {
   return (
     <>
+      <Custom_Theme />
       <Routers />
-      <Theme />
-      <TextArea />
     </>
   )
 }
