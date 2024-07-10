dc97d36f8ce4c56b29a18301fa85b7296ecbb8f2
WhiteBearM <bhs9641@gmail.com>
2024-07-10 16:11:19 +0900

    daisyUI component Props로 id값 동적으로 설정하기

--- nowa/src/components/TextArea/textArea.tsx
+++ nowa/src/components/TextArea/textArea.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 
-const textArea = () => {
+interface TextArea {
+  id: string
+}
+
+const textArea: React.FC<TextArea> = ({ id }) => {
   return (
     <div>
       <textarea
+        id={id}
         className="textarea textarea-bordered resize-none"
         placeholder="Bio"
       />
-      {/* ddddd */}
     </div>
   )
 }
