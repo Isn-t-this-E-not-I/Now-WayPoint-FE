04a4de86393393b037a05ceb2531ea87ece21bd2
525tea <jene0028@gmail.com>
2024-07-11 19:04:42 +0900

    포트 연결

--- nowa/src/api/userApi.ts
+++ nowa/src/api/userApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000/api/user'; // 서버 URL
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000/api';
 
 interface LoginPayload {
   email: string;
