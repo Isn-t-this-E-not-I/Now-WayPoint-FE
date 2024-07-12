b8870ed455161db1e77fde1b26d0a229b54cf1f3
WhiteBearM <bhs9641@gmail.com>
2024-07-12 14:10:12 +0900

    kakaoMap 백엔드 연동 구현중

--- nowa/src/api/KaKaomap/kakaomap.tsx
+++ nowa/src/api/KaKaomap/kakaomap.tsx
@@ -1,10 +1,17 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'http://localhost:8080/api'
+const API_BASE_URL = 'http://localhost:8080/map'
 
 export const getMapData = async (): Promise<any> => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/경로`)
+    const tokenResponse = await axios.get(`${API_BASE_URL}/token`) // 토큰을 받아오는 엔드포인트
+    const token = tokenResponse.data.token
+
+    const response = await axios.get(`${API_BASE_URL}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
     return response.data
   } catch (error) {
     console.error('Failed to fetch map data:', error)
