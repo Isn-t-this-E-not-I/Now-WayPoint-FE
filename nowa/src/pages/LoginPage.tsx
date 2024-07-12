9d55ea18bfe6b388a7553650705917c3aad9da13
immyeong <wpfls6670@naver.com>
2024-07-12 10:29:33 +0900

    💡refactor: login, register 리팩토링

--- nowa/src/pages/LoginPage.tsx
+++ nowa/src/pages/LoginPage.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import { login, loginWithKakao } from '../api/userApi';
-import TextInput from '../components/TextInput/TextInput';
+import TextInput from '../components/TextInput/textInput';
 // import ThemeController from '../components/ThemeController/ThemeController';
 import { useNavigate } from 'react-router-dom';
 
 
 const LoginPage: React.FC = () => {
-  const [email, setEmail] = useState<string>('');
+  const [loginId, setLoginId] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const [rememberMe, setRememberMe] = useState<boolean>(false);
   const [error, setError] = useState<string>('');
@@ -14,7 +14,7 @@ const LoginPage: React.FC = () => {
 
   const handleLogin = async () => {
     try {
-      const data = await login({ email, password });
+      const data = await login({ loginId, password });
       console.log('Login success:', data);
       navigate('/main');
     } catch (error) {
@@ -25,8 +25,8 @@ const LoginPage: React.FC = () => {
 
   const handleKakaoLogin = async () => {
     try {
-      const data = await loginWithKakao();
-      window.location.href = data.url; // 카카오 로그인 페이지로 리디렉트
+      // const data = await loginWithKakao();
+      window.location.href = 'http://localhost:8080/api/user/login/kakao'; // 카카오 로그인 페이지로 리디렉트 data.url
     } catch (error) {
       console.error('Kakao login failed:', error);
     }
@@ -40,7 +40,7 @@ const LoginPage: React.FC = () => {
     <div className="flex flex-col items-center justify-center min-h-screen bg-white">
       <div className="card w-96 shadow-xl p-5 bg-gray-50">
         <h2 className="text-lg font-bold mb-4">로그인</h2>
-        <TextInput type="email" placeholder="이메일" onChange={e => setEmail(e.target.value)} value={email} className="mb-4" />
+        <TextInput type="email" placeholder="이메일" onChange={e => setLoginId(e.target.value)} value={loginId} className="mb-4" />
         <TextInput type="password" placeholder="비밀번호" onChange={e => setPassword(e.target.value)} value={password} className="mb-4" />
         <label className="flex items-center space-x-2">
           <input type="checkbox" checked={rememberMe} onChange={() => setRememberMe(!rememberMe)} className="checkbox checkbox-primary" />
--- nowa/src/pages/RegisterPage.tsx
+++ nowa/src/pages/RegisterPage.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import TextInput from '../components/TextInput/TextInput';  // 올바른 경로로 수정
+import TextInput from '../components/TextInput/textInput';  // 올바른 경로로 수정
 import { register } from '../api/userApi';
 
 const RegisterPage: React.FC = () => {
-  const [email, setEmail] = useState('');
+  const [loginId, setLoginId] = useState('');
   const [password, setPassword] = useState('');
   const [name, setName] = useState('');
   const [nickname, setNickname] = useState('');
@@ -12,11 +12,17 @@ const RegisterPage: React.FC = () => {
 
   const handleRegister = async () => {
     try {
-      const response = await register({ email, password, name, nickname });
-      if (response.success) {
+      const response = await register({ loginId, password, name, nickname });
+      if (response.data === "ok") {
         navigate('/login'); // 회원가입 성공 시 로그인 페이지로 리디렉션
-      } else {
-        alert('회원가입 실패: ' + response.message);
+      } else{
+        if(response.data === "idNo"){
+          alert('아이디가 중복되었습니다.');
+        }else if(response.data === "nicknameNo"){
+          alert('닉네임이 중복되었습니다.');
+        }else if(response.data === "idNicknameNo"){
+          console.log(response.message)
+        }
       }
     } catch (error) {
       console.error('Registration error:', error);
@@ -32,7 +38,7 @@ const RegisterPage: React.FC = () => {
     <div className="flex flex-col items-center justify-center min-h-screen bg-white">
       <div className="card w-96 shadow-xl p-5">
         <h2 className="text-lg font-bold mb-4">회원가입</h2>
-        <TextInput type="email" placeholder="이메일" onChange={e => setEmail(e.target.value)} value={email} className="mb-4" />
+        <TextInput type="email" placeholder="이메일" onChange={e => setLoginId(e.target.value)} value={loginId} className="mb-4" />
         <TextInput type="password" placeholder="비밀번호" onChange={e => setPassword(e.target.value)} value={password} className="mb-4" />
         <TextInput type="text" placeholder="이름" onChange={e => setName(e.target.value)} value={name} className="mb-4" />
         <TextInput type="text" placeholder="닉네임" onChange={e => setNickname(e.target.value)} value={nickname} className="mb-4" />
