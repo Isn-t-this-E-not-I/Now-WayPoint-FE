49b3c5bcedc405ba4b4f34625e2489b761cdaacb
SUN <siksik2782259@gmail.com>
2024-07-15 15:13:25 +0900

    테스트중

--- nowa/src/api/KaKaomap/kakaomap.tsx
+++ nowa/src/api/KaKaomap/kakaomap.tsx
@@ -1,13 +1,16 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'http://localhost:8080/api'
+const API_BASE_URL = 'http://localhost:8080/api/map'
 
 export const getMapData = async (): Promise<any> => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/경로`)
+    const response = await axios.get(`${API_BASE_URL}`)
     return response.data
   } catch (error) {
     console.error('Failed to fetch map data:', error)
     throw new Error('Failed to fetch map data')
+    /** 테스트중 */
   }
+
+  
 }
