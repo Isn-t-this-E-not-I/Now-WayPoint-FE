59195fe27b526708859e55ebcdbdf3c85039e027
MEANING-K <blossom9704@gmail.com>
2024-07-23 17:24:48 +0900

    update : chattingPage의 ReusableModalContent 삭제

--- nowa/src/pages/Chat/chattingPage.tsx
+++ nowa/src/pages/Chat/chattingPage.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import Modal from '../../components/Modal/modal'
 import ChatBubble from '../../components/ChatBubble/chatBubble'
-import ReusableModalContent from '../../components/ReusableModalContent/reusableModalContent'
 import useModal from '@/hooks/modal'
 import {
   enterChatRoom,
