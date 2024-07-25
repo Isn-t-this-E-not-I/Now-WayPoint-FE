cf1cf00c924451a4e06550e3c5ff275f9c63b3f2
WhiteBearM <bhs9641@gmail.com>
2024-07-25 14:01:23 +0900

    stomp 구독 위치 정보 및 카테고리 ui 구현

--- nowa/src/api/KaKaomap/kakaomain.tsx
+++ nowa/src/api/KaKaomap/kakaomain.tsx
@@ -1,8 +1,9 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import { getKakaoApiData } from '@/api/KaKaomap/kakaomap'
 import { useLocation } from 'react-router-dom'
 import SockJS from 'sockjs-client'
-import { Client } from '@stomp/stompjs'
+import { Client, IMessage } from '@stomp/stompjs'
+import '@/styles/kakaomap.css'
 
 declare global {
   interface Window {
@@ -15,6 +16,63 @@ const MainPage: React.FC = () => {
   const location = useLocation()
   const token = localStorage.getItem('token')
   const nickname = localStorage.getItem('nickname')
+  const [locate, setLocate] = useState('')
+  const [stompClient, setStompClient] = useState<Client | null>(null)
+  const [isConnected, setIsConnected] = useState(false)
+
+  useEffect(() => {
+    const sock = new SockJS('http://15.165.236.244:8080/main')
+    console.log(token)
+
+    const client = new Client({
+      webSocketFactory: () => sock,
+      connectHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+      onConnect: (frame) => {
+        console.log('Websocket connected!')
+        setIsConnected(true) // Update connection status
+        client.subscribe(
+          `/queue/notify/${nickname}`,
+          (messageOutput: IMessage) => {
+            console.log(messageOutput.body)
+          }
+        )
+        client.subscribe(
+          `/topic/follower/${nickname}`,
+          (messageOutput: IMessage) => {
+            console.log(messageOutput.body)
+          }
+        )
+        client.subscribe(
+          `/topic/${locate}/${nickname}`,
+          (messageOutput: IMessage) => {
+            console.log(messageOutput.body)
+          }
+        )
+      },
+      onDisconnect: () => {
+        console.log('Websocket disconnected!')
+        setIsConnected(false) // Update connection status
+      },
+      onStompError: (frame) => {
+        console.error('Broker reported error: ' + frame.headers['message'])
+        console.error('Additional details: ' + frame.body)
+      },
+      debug: (str) => {
+        console.log('STOMP Debug:', str)
+      },
+    })
+
+    setStompClient(client)
+
+    client.activate()
+
+    return () => {
+      client.deactivate()
+      console.log('Websocket disconnected')
+    }
+  }, [token, nickname, locate])
 
   const initializeMap = (latitude: number, longitude: number) => {
     const script = document.createElement('script')
@@ -39,18 +97,20 @@ const MainPage: React.FC = () => {
   }
 
   useEffect(() => {
-    //지도 출력
+    // 지도 출력
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         async (position) => {
-          const latitude = position.coords.latitude
-          const longitude = position.coords.longitude
+          const latitude = position.coords.latitude //위도
+          const longitude = position.coords.longitude //경도
 
           try {
             // const mapData =
             await getKakaoApiData(`${latitude},${longitude}`)
 
             initializeMap(latitude, longitude)
+
+            setLocate(`${latitude},${longitude}`)
           } catch (error) {
             console.error('지도 초기화 실패:', error)
           }
@@ -62,55 +122,52 @@ const MainPage: React.FC = () => {
     } else {
       console.error('Geolocation을 지원하지 않는 브라우저입니다.')
     }
+  }, [])
 
-    // SockJS와 Stomp 클라이언트 구성
-    if (!token) {
-      console.log('No token provided')
-      return
+  const selectCategory = (category: string) => {
+    if (stompClient && isConnected) {
+      const response = stompClient.publish({
+        destination: '/app/main/category',
+        body: JSON.stringify({ category: category }),
+      })
+    } else {
+      console.error('Not connected to WebSocket')
     }
-
-    const sock = new SockJS('http://15.165.236.244:8080/main')
-    console.log(token)
-    const stompClient = new Client({
-      webSocketFactory: () => sock,
-      connectHeaders: {
-        Authorization: `Bearer ${token}`,
-      },
-      onConnect: () => {
-        console.log('Websocket connected!')
-        stompClient.subscribe(
-          `/queue/notify/${nickname}`,
-          function (messageOutput) {
-            console.log(messageOutput.body)
-          }
-        )
-        stompClient.subscribe(
-          `/topic/follower/${nickname}`,
-          function (messageOutput) {
-            console.log(messageOutput.body)
-          }
-        )
-      },
-      onStompError: (frame) => {
-        console.error('Broker reported error: ' + frame.headers['message'])
-        console.error('Additional details: ' + frame.body)
-      },
-      debug: (str) => {
-        console.log('STOMP Debug:', str)
-      },
-    })
-
-    stompClient.activate()
-
-    return () => {
-      stompClient.deactivate()
-      console.log('Websocket disconnected')
-    }
-  }, [token, nickname])
+  }
 
   return (
     <div>
       <div ref={mapContainer} style={{ width: '100%', height: '100vh' }} />
+      <div id="categoryBox">
+        <button
+          className="categoryButtons"
+          id="categorybtn1"
+          onClick={() => selectCategory('PHOTO')}
+        >
+          식당
+        </button>
+        <button
+          className="categoryButtons"
+          id="categorybtn2"
+          onClick={() => selectCategory('VIDEO')}
+        >
+          카페
+        </button>
+        <button
+          className="categoryButtons"
+          id="categorybtn3"
+          onClick={() => selectCategory('MP3')}
+        >
+          편의점
+        </button>
+        <button
+          className="categoryButtons"
+          id="categorybtn4"
+          onClick={() => selectCategory('ALL')}
+        >
+          전체
+        </button>
+      </div>
     </div>
   )
 }
