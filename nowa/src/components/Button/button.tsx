5b82f59f21f5ac65340ba79e9e1641505cf0ce7a
525tea <jene0028@gmail.com>
2024-07-26 18:30:26 +0900

    컴포넌트 수정

--- nowa/src/components/Button/button.tsx
+++ nowa/src/components/Button/button.tsx
@@ -1,21 +1,3 @@
-// import React from 'react'
-
-// interface ButtonProps {
-//   id: string
-// }
-
-// const button: React.FC<ButtonProps> = ({ id }) => {
-//   return (
-//     <div>
-//       <button id={id} className="btn">
-//         Button
-//       </button>
-//     </div>
-//   )
-// }
-
-// export default button
-
 import React from 'react'
 
 interface ButtonProps {
--- nowa/src/components/FileInput/fileInput.tsx
+++ nowa/src/components/FileInput/fileInput.tsx
@@ -19,23 +19,3 @@ const FileInput: React.FC<FileInputProps> = ({ id, onChange }) => {
 };
 
 export default FileInput;
-
-// import React from 'react'
-
-// interface FileInput {
-//   id: string
-// }
-
-// const fileInput: React.FC<FileInput> = ({ id }) => {
-//   return (
-//     <div>
-//       <input
-//         id={id}
-//         type="file"
-//         className="file-input file-input-bordered w-full max-w-xs"
-//       />
-//     </div>
-//   )
-// }
-
-// export default fileInput
