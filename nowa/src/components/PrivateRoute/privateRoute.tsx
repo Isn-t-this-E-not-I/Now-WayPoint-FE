518211e1c0daf9fa4b60e57d7153d47bec51d447
WhiteBearM <bhs9641@gmail.com>
2024-07-30 10:50:48 +0900

    일반로그인 카카오로그인 토큰이 존재하지않으면 로그인페이지 라우트

--- nowa/src/components/PrivateRoute/privateRoute.tsx
+++ nowa/src/components/PrivateRoute/privateRoute.tsx
@@ -3,8 +3,9 @@ import { Navigate, Outlet } from 'react-router-dom'
 
 const PrivateRoute: React.FC = () => {
   const token = localStorage.getItem('token')
+  const cookie_token = document.cookie
 
-  return token ? <Outlet /> : <Navigate to="/login" />
+  return token && cookie_token ? <Outlet /> : <Navigate to="/login" />
 }
 
 export default PrivateRoute
