7030710947779989421344a950c8043b69a8be38
WhiteBearM <bhs9641@gmail.com>
2024-07-30 11:53:02 +0900

    카카오 토큰 로컬스토리지 덮어쓰기 구현 완

--- nowa/src/components/PrivateRoute/privateRoute.tsx
+++ nowa/src/components/PrivateRoute/privateRoute.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import { Navigate, Outlet } from 'react-router-dom'
+// import React from 'react'
+// import { Navigate, Outlet } from 'react-router-dom'
 
-const PrivateRoute: React.FC = () => {
-  const token = localStorage.getItem('token')
-  const cookie_token = document.cookie
+// const PrivateRoute: React.FC = () => {
+//   const token = localStorage.getItem('token')
+//   const cookie_token = document.cookie
 
-  return token && cookie_token ? <Outlet /> : <Navigate to="/login" />
-}
+//   return token && cookie_token ? <Outlet /> : <Navigate to="/login" />
+// }
 
-export default PrivateRoute
+// export default PrivateRoute
--- nowa/src/routes/index.tsx
+++ nowa/src/routes/index.tsx
@@ -10,7 +10,7 @@ import FindIdPage from '@/pages/FindIdPage'
 import FindPasswordPage from '@/pages/FindPasswordPage'
 import DetailContent from '@/pages/DetailContent/detailContent'
 import UploadContent from '@/pages/MakeContent/makeContent'
-import PrivateRoute from '@/components/PrivateRoute/privateRoute'
+// import PrivateRoute from '@/components/PrivateRoute/privateRoute'
 
 const Routers: React.FC = () => {
   return (
@@ -19,19 +19,19 @@ const Routers: React.FC = () => {
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/find-id" element={<FindIdPage />} />
       <Route path="/find-password" element={<FindPasswordPage />} />
-      <Route element={<PrivateRoute />}>
-        <Route path="/" element={<IndexPage />} />
-        <Route path="/main" element={<MainPage />} />
-        <Route path="/memberfind" element={<></>} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/UploadContent" element={<UploadContent />} />
-        <Route path="/detailContent/:id" element={<DetailContent />} />
-        <Route path="/contentDetail" element={<></>} />
-        <Route path="/contentEdit" element={<></>} />
-        <Route path="/profileEdit" element={<></>} />
-        <Route path="/chat" element={<></>} />
-        <Route path="/notification" element={<></>} />
-      </Route>
+      {/* <Route element={<PrivateRoute />}> */}
+      <Route path="/" element={<IndexPage />} />
+      <Route path="/main" element={<MainPage />} />
+      <Route path="/memberfind" element={<></>} />
+      <Route path="/mypage" element={<MyPage />} />
+      <Route path="/UploadContent" element={<UploadContent />} />
+      <Route path="/detailContent/:id" element={<DetailContent />} />
+      <Route path="/contentDetail" element={<></>} />
+      <Route path="/contentEdit" element={<></>} />
+      <Route path="/profileEdit" element={<></>} />
+      <Route path="/chat" element={<></>} />
+      <Route path="/notification" element={<></>} />
+      {/* </Route> */}
     </Routes>
   )
 }
