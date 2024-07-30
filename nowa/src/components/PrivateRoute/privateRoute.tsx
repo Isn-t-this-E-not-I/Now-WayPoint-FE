73833426dc010ed538b4aa3685fdbb45ef336be0
WhiteBearM <bhs9641@gmail.com>
2024-07-30 19:36:32 +0900

    privateRoute edit

--- nowa/src/components/PrivateRoute/privateRoute.tsx
+++ nowa/src/components/PrivateRoute/privateRoute.tsx
@@ -2,16 +2,9 @@ import React from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
 
 const PrivateRoute: React.FC = () => {
-<<<<<<< HEAD
   const token = localStorage.getItem('token')
-  const cookie_token = document.cookie
 
   return token ? <Outlet /> : <Navigate to="/login" />
-=======
-    const token = localStorage.getItem('token')
-
-    return token ? <Outlet /> : <Navigate to="/login" />
->>>>>>> 0f3ac01f177f4e78644581a05a8f419c8be10099
 }
 
 export default PrivateRoute
