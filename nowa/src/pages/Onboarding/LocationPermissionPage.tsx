e586803bd6a24f4ad46d0efc7944f26a2c16d40a
525tea <jene0028@gmail.com>
2024-08-09 10:19:37 +0900

    locationpermission 디자인 수정

--- nowa/src/pages/Onboarding/LocationPermissionPage.tsx
+++ nowa/src/pages/Onboarding/LocationPermissionPage.tsx
@@ -1,11 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from '../../components/Button/button';
+import { FaMapMarkerAlt } from 'react-icons/fa';
+import './styles.css';
 
 const LocationPermissionPage: React.FC = () => {
   const [locationError, setLocationError] = useState('');
   const [nickname, setNickname] = useState('');
   const [isModalOpen, setIsModalOpen] = useState(true);
+  const [animate, setAnimate] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -32,12 +35,35 @@ const LocationPermissionPage: React.FC = () => {
     }
   };
 
+  useEffect(() => {
+    setAnimate(true);
+  }, []);
+
+  const getRandomPosition = () => {
+    const top = Math.floor(Math.random() * 50) + 10; // 10%에서 60% 사이의 랜덤한 위치
+    const left = Math.floor(Math.random() * 80) + 10; // 10%에서 90% 사이의 랜덤한 위치
+    return { top: `${top}%`, left: `${left}%` };
+  };
+
   return (
     <div className="relative min-h-screen bg-cover bg-center" style={{ backgroundImage: "url('/images/Locationbackground2.png')" }}>
       <div className="absolute inset-0 flex items-start justify-center bg-black bg-opacity-30 pt-64">
         <h2 className="text-2xl font-bold text-white mt-8">{nickname}님, 위치를 연결할까요?</h2>
       </div>
-        <div className="relative z-50 flex flex-col items-center justify-center min-h-screen">
+      <div className="relative z-50 flex items-center justify-center min-h-screen">
+        <div className="flex flex-col items-center justify-center">
+          <div className="relative w-full h-full">
+            {[1, 2, 3].map((index) => {
+              const position = getRandomPosition();
+              return (
+                <FaMapMarkerAlt
+                  key={index}
+                  className={`absolute w-16 h-16 text-red-500 ${animate ? 'drop-animation' : ''}`}
+                  style={{ top: position.top, left: position.left }}
+                />
+              );
+            })}
+          </div>
           <Button
             onClick={handleLocationPermission}
             className="btn-primary text-base mt-16 w-64 h-14 bg-yellow-300 hover:bg-yellow-400 border-none"
@@ -47,12 +73,12 @@ const LocationPermissionPage: React.FC = () => {
           {locationError && (
             <div className="text-red-500 mt-4">
               {locationError}
-        </div>
+            </div>
           )}
         </div>
+      </div>
     </div>
   );
 };
-
 
 export default LocationPermissionPage;
