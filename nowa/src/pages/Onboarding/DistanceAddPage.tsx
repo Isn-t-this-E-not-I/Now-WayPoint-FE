785dfb66dbf2f645b047477da9cfa340938ea2b6
525tea <jene0028@gmail.com>
2024-08-14 01:41:14 +0900

    distance-add에서 로그인 유저 제외

--- nowa/src/pages/Onboarding/DistanceAddPage.tsx
+++ nowa/src/pages/Onboarding/DistanceAddPage.tsx
@@ -24,7 +24,7 @@ const DistanceAddPage: React.FC = () => {
       try {
         const token = localStorage.getItem('token');
         if (token) {
-          const location = import.meta.env.VITE_APP_API
+          const location = import.meta.env.VITE_APP_API;
           const response = await fetch(`${location}/user/locate`, {
             method: 'GET',
             headers: {
@@ -33,61 +33,60 @@ const DistanceAddPage: React.FC = () => {
             },
           });
 
-            const textResponse = await response.text();
-            console.log('Raw Response:', textResponse);
+          const textResponse = await response.text();
+          console.log('Raw Response:', textResponse);
 
-            const users = JSON.parse(textResponse);
-            console.log('Parsed JSON:', users);
+          const users = JSON.parse(textResponse);
+          console.log('Parsed JSON:', users);
 
-            const filteredUsers = users.filter((user: any) => user.nickname !== nickname);
+          // 로그인한 유저를 추천 친구 목록에서 제외
+          const filteredUsers = users.filter((user: any) => user.nickname !== nickname);
 
-            const haversineDistance = (lat1: number, lon1: number, lat2: number, lon2: number) => {
-              const R = 6371; // Earth's radius in km
-              const dLat = (lat2 - lat1) * Math.PI / 180;
-              const dLon = (lon2 - lon1) * Math.PI / 180;
-              const a =
-                Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-                Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
-                Math.sin(dLon / 2) * Math.sin(dLon / 2);
-              const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-              const distance = R * c; // Distance in km
-              return distance;
-            };
-            
-            const mappedUsers = filteredUsers.map((user: any) => {
-              let calculatedDistance = NaN;
-              let distance;
-            
-              // Extract user location
-              if (locate != null) {
-                const [userLongitude, userLatitude] = locate.split(',').map(coord => parseFloat(coord.trim()));
-            
-                // Extract post location
-                const [postLongitude, postLatitude] = user.locate.split(',').map((coord: string) => parseFloat(coord.trim()));
-            
-                // Calculate distance between user and post
-                calculatedDistance = haversineDistance(userLatitude, userLongitude, postLatitude, postLongitude);
-            
-                // Round to three decimal places and convert to string with 'km'
-                calculatedDistance = Math.floor(calculatedDistance);
-                
-                // Convert distances less than 1km to "0km"
-                if (calculatedDistance < 1) {
-                  distance = "0km";
-                } else {
-                  distance = `${calculatedDistance}km`;
-                }
+          const haversineDistance = (lat1: number, lon1: number, lat2: number, lon2: number) => {
+            const R = 6371; // Earth's radius in km
+            const dLat = (lat2 - lat1) * Math.PI / 180;
+            const dLon = (lon2 - lon1) * Math.PI / 180;
+            const a =
+              Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+              Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+              Math.sin(dLon / 2) * Math.sin(dLon / 2);
+            const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+            const distance = R * c; // Distance in km
+            return distance;
+          };
+
+          const mappedUsers = filteredUsers.map((user: any) => {
+            let calculatedDistance = NaN;
+            let distance;
+
+            // Extract user location
+            if (locate != null) {
+              const [userLongitude, userLatitude] = locate.split(',').map(coord => parseFloat(coord.trim()));
+
+              // Extract post location
+              const [postLongitude, postLatitude] = user.locate.split(',').map((coord: string) => parseFloat(coord.trim()));
+
+              // Calculate distance between user and post
+              calculatedDistance = haversineDistance(userLatitude, userLongitude, postLatitude, postLongitude);
+
+              // Round to three decimal places and convert to string with 'km'
+              calculatedDistance = Math.floor(calculatedDistance);
+
+              // Convert distances less than 1km to "0km"
+              if (calculatedDistance < 1) {
+                distance = "0km";
+              } else {
+                distance = `${calculatedDistance}km`;
               }
-            
-              return {
-                name: user.name,
-                nickname: user.nickname,
-                profileImageUrl: user.profileImageUrl,
-                locate: distance
-              };
-            });
+            }
 
-
+            return {
+              name: user.name,
+              nickname: user.nickname,
+              profileImageUrl: user.profileImageUrl,
+              locate: distance
+            };
+          });
 
           setRecommendedFriends(mappedUsers);
           console.log('Mapped Users:', mappedUsers);
@@ -136,62 +135,61 @@ const DistanceAddPage: React.FC = () => {
     }
   }, []);
 
-
   return (
     <div className="relative min-h-screen bg-cover bg-center bg-image">
       <h2 className="text-xl font-bold mb-12 text-gray-800 text-center pt-40">
         {nickname}님의 위치를 기반으로 주변에 있는 유저를 추천드릴게요
       </h2>
       <div className="flex justify-center space-x-8">
-      <div className="bg-white shadow-md rounded-lg p-6 max-h-100" style={{ width: '360px' }}>
-        <h3 className="text-base font-semibold text-center mb-4">나와 가까운 유저</h3>
-        <ul className="overflow-y-auto max-h-80">
-          {recommendedFriends.map((user: User) => (
-            <li key={user.nickname} className="flex items-center justify-between p-4 bg-blue-50 rounded-lg mb-2">
-              <div className="flex items-center">
-                <img 
-                  src={user.profileImageUrl} 
-                  alt={user.name} 
-                  className="w-10 h-10 rounded-full mr-4" 
-                />
-                <div className="text-sm text-black">
-                  <p>{user.nickname} ({user.name})</p>
-                  <p className="text-gray-500 text-sm">{user.locate}</p>
+        <div className="bg-white shadow-md rounded-lg p-6 max-h-100" style={{ width: '360px' }}>
+          <h3 className="text-base font-semibold text-center mb-4">나와 가까운 유저</h3>
+          <ul className="overflow-y-auto max-h-80">
+            {recommendedFriends.map((user: User) => (
+              <li key={user.nickname} className="flex items-center justify-between p-4 bg-blue-50 rounded-lg mb-2">
+                <div className="flex items-center">
+                  <img 
+                    src={user.profileImageUrl} 
+                    alt={user.name} 
+                    className="w-10 h-10 rounded-full mr-4" 
+                  />
+                  <div className="text-sm text-black">
+                    <p>{user.nickname} ({user.name})</p>
+                    <p className="text-gray-500 text-sm">{user.locate}</p>
+                  </div>
                 </div>
-              </div>
-              <FaPlus
-                onClick={() => handleAddFriend(user)}
-                className="text-blue-500 cursor-pointer"
-              />
-            </li>
-          ))}
-        </ul>
+                <FaPlus
+                  onClick={() => handleAddFriend(user)}
+                  className="text-blue-500 cursor-pointer"
+                />
+              </li>
+            ))}
+          </ul>
         </div>
         <div className="bg-white shadow-md rounded-lg p-6 max-h-100" style={{ width: '360px' }}>
-        <h3 className="text-base font-semibold text-center mb-4">선택된 유저</h3>
-        <ul className="overflow-y-auto max-h-80">
-          {selectedFriends.map((friend: User) => (
-            <li key={friend.nickname} className="flex items-center justify-between p-4 bg-blue-100 rounded-lg mb-2">
-              <div className="flex items-center">
-                <img 
-                  src={friend.profileImageUrl} 
-                  alt={friend.name} 
-                  className="w-10 h-10 rounded-full mr-4" 
-                />
-                <div className="text-sm text-black">
-                  <p>{friend.nickname} ({friend.name})</p>
-                  <p className="text-gray-500 text-sm">{friend.locate}</p>
+          <h3 className="text-base font-semibold text-center mb-4">선택된 유저</h3>
+          <ul className="overflow-y-auto max-h-80">
+            {selectedFriends.map((friend: User) => (
+              <li key={friend.nickname} className="flex items-center justify-between p-4 bg-blue-100 rounded-lg mb-2">
+                <div className="flex items-center">
+                  <img 
+                    src={friend.profileImageUrl} 
+                    alt={friend.name} 
+                    className="w-10 h-10 rounded-full mr-4" 
+                  />
+                  <div className="text-sm text-black">
+                    <p>{friend.nickname} ({friend.name})</p>
+                    <p className="text-gray-500 text-sm">{friend.locate}</p>
+                  </div>
                 </div>
-              </div>
-              <FaCheck
-                onClick={() => handleRemoveFriend(friend.nickname)}
-                className="text-green-500 cursor-pointer"
-              />
-            </li>
-          ))}
-        </ul>
+                <FaCheck
+                  onClick={() => handleRemoveFriend(friend.nickname)}
+                  className="text-green-500 cursor-pointer"
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
       <div className="flex justify-center mt-8">
         <Button
           className="btn-primary text-base mt-4 w-64 h-14 bg-yellow-400 text-white hover:bg-yellow-300 border-none"
